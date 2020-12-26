d4dafb6b63474d33e5970dbbb68d2cce69fdb6c8
AlexieVQ <alexie@nephos.me>
2020-12-26 21:30:42 +0100

    Updated RandTextCore

--- lib/rand_text_core.rb
+++ lib/rand_text_core.rb
@@ -1,5 +1,23 @@
+# Main class for the +rand_text_core+ gem.
+#
+# Class for generators objects. A generator object is created for a given
+# directory.
 class RandTextCore
-	def self.hi
-		puts "RandTextCore gem"
+	# @return [Array<String>] Files' names
+	attr_reader :files
+
+	# Creates a new generator for CSV files stored into directory of given path.
+	#
+	# @param path [#to_str] directory's path, ending with a +'/'+, or not
+	# @raise [TypeError] +path+ is not a String
+	def initialize(path)
+		begin
+			path = path.to_str
+		rescue NoMethodError
+			raise TypeError,
+				"no implicit conversion of #{path.class} into String"
+		end
+		path += path[-1] == '/' ? '' : '/'
+		@files = Dir.glob(path + '*.csv')
 	end
 end
