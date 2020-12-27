06fe19f13079006c366f68f211b2c00954413d10
AlexieVQ <alexie@nephos.me>
2020-12-28 00:02:53 +0100

    Added coverage

--- test/test_rand_text_core.rb
+++ test/test_rand_text_core.rb
@@ -1,22 +1,25 @@
 #!/usr/bin/env ruby
 
+require 'simplecov'
+SimpleCov.start
+
 require 'test/unit'
 require_relative '../lib/rand_text_core'
 
 class TestRandTextCore < Test::Unit::TestCase
 
 	def test_files_no_slash
-		path = 'dir1'
+		path = 'test/dir1'
 		files = ['table1.csv', 'table2.csv'].map { |f| path + '/' + f }.sort
 		core = RandTextCore.new(path)
-		assert_equal(core.files.sort, files)
+		assert_equal(files, core.files.sort)
 	end
 
 	def test_files_slash
-		path = 'dir1/'
+		path = 'test/dir1/'
 		files = ['table1.csv', 'table2.csv'].map { |f| path + f }.sort
 		core = RandTextCore.new(path)
-		assert_equal(core.files.sort, files)
+		assert_equal(files, core.files.sort)
 	end
 
 	def test_path_type
