e5357ec1f87c5a5e15c13c78cf3ce1ccb75fb8cb
AlexieVQ <alexie@nephos.me>
2021-01-03 18:52:22 +0100

    Corrections on SymbolTable

--- lib/rand_text_core/symbol_table.rb
+++ lib/rand_text_core/symbol_table.rb
@@ -35,10 +35,12 @@ class RandTextCore::SymbolTable
 	attr_reader :rules
 
 	# Creates a new symbol table.
-	# @param [Hash{Symbol => Proc}] functions hash map storing functions used in
-	#  the expansion language
+	# @param [Hash{#to_sym => #to_proc}] functions hash map storing functions
+	#  used in the expansion language (must be lambdas)
 	# @param [Enumerable<Class>] rules set of rules
 	# @raise [TypeError] wrong type of parameters
+	# @raise [ArgumentError] function not a lambda, or class not a subclass of
+	#  RuleVariant
 	# @raise [RuntimeError] rules with the same name
 	def initialize(functions, rules)
 		unless functions.kind_of?(Hash)
@@ -46,17 +48,28 @@ class RandTextCore::SymbolTable
 				"wrong type for first argument (Hash{Symbol=>Proc} expected, " +
 				"#{functions.class} given)"
 		end
-		functions.each do |symbol, function|
-			unless symbol.kind_of?(Symbol)
+		@functions = functions.each_with_object({}) do |pair, hash|
+			symbol = nil
+			function = nil
+			begin
+				symbol = pair[0].to_sym
+			rescue NoMethodError
 				raise TypeError,
 					"wrong type of key in first argument (Symbol expected, " +
-					"#{symbol.class} given)"
+					"#{pair[0].class} given)"
 			end
-			unless function.kind_of?(Proc)
+			begin
+				function = pair[1].to_proc
+			rescue NoMethodError
 				raise TypeError,
 					"wrong type of value for key #{symbol} in first argument " +
-					"(Proc expected, #{function} given)"
+					"(Proc expected, #{pair[1].class} given)"
 			end
+			unless function.lambda?
+				raise ArgumentError,
+					"function #{symbol} is not a lambda"
+			end
+			hash[symbol] = function
 		end
 		unless rules.kind_of?(Enumerable)
 			raise TypeError,
@@ -64,18 +77,17 @@ class RandTextCore::SymbolTable
 				"#{rules.class} given)"
 		end
 		rules.each_with_index do |rule, i|
-			unless rule.kind_of(Class)
+			unless rule.kind_of?(Class)
 				raise TypeError,
 					"wrong type for object of index #{i} in second argument " +
 					"(Class espected, #{rule.class} given"
 			end
 			unless rule.superclass == RandTextCore::RuleVariant
-				raise TypeError,
+				raise ArgumentError,
 					"class of index #{i} in second argument is not a subclass" +
 					" of RandTextCore::RuleVariant"
 			end
 		end
-		@functions = functions.clone.freeze
 		@rules = rules.each_with_object({}) do |rule, hash|
 			if hash[rule.rule_name]
 				raise "two rules with the same name #{rule.rule_name.inspect}"
@@ -157,6 +169,7 @@ class RandTextCore::SymbolTable
 	# @return function's return
 	# @raise [TypeError] wrong types
 	# @raise [KeyError] no function of given name
+	# @raise [ArgumentError] wrong number of arguments
 	def call(name, *args)
 		begin
 			name = name.to_sym
@@ -172,7 +185,7 @@ class RandTextCore::SymbolTable
 					"no implicit conversion of #{arg.class} into String"
 			end
 		end
-		@functions[name].call(*args, self)
+		@functions.fetch(name).call(*args, self)
 	end
 
 	# Add a variable with given name that stores given value.
@@ -212,6 +225,7 @@ class RandTextCore::SymbolTable
 	# @param [Snapshot] snapshot snapshot to restore
 	# @return [self]
 	# @raise [TypeError] wrong argument type
+	# @raise [ArgumentError] snapshot of the wrong table
 	def restore(snapshot)
 		unless snapshot.kind_of?(Snapshot)
 			raise TypeError,
