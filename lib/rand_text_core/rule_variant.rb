40ca3ce11e1e74ee56116f3861a487bf4a2df40e
AlexieVQ <alexie@nephos.me>
2021-02-12 18:23:17 +0100

    Added Weight attribute type to any rule

--- lib/rand_text_core/rule_variant.rb
+++ lib/rand_text_core/rule_variant.rb
@@ -236,7 +236,6 @@ class RandTextCore::RuleVariant
 				@attr_types[attribute] =
 					RandTextCore::DataTypes::Identifier.type
 			elsif attribute == :weight
-				@attr_types[attribute] = RandTextCore::DataTypes::Weight.type
 				define_method(:default_weight) do
 					RandTextCore::DataTypes::Weight.type.convert(
 						@attributes[:weight],
@@ -263,6 +262,7 @@ class RandTextCore::RuleVariant
 		unless @attr_types[:weight]
 			define_method(:default_weight) { 1 }
 		end
+		@attr_types[attribute] = RandTextCore::DataTypes::Weight.type
 		private(:default_weight)
 		@attr_types.freeze
 		nil
